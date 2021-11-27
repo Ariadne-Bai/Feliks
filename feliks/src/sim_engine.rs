d87f05867513a553027487670f302c3555c4d06f
Ariadne Bai <bairuoqi@yeah.net>
2021-11-27 04:29:24 -0500

    two lines train test also pass  :)

--- feliks/src/sim_engine.rs
+++ feliks/src/sim_engine.rs
@@ -97,11 +97,64 @@ mod test {
             simengine.do_step(clock);
             clock += 1;
         }
-        // set up a line
-        // pop up the line with 3 stations
-        // pop up the line with 2 trains for the day
-        // spawn trains for the line
-        // do_step and see what got pushed/consumed ax
     }
     
+    #[test]
+    fn TwoLineTrains() {
+        let mut sch = Scheduler::new();
+        let mut trmanager = TrainManager::new();
+        let mut simengine = SimEngine::new(&mut sch, &mut trmanager);
+        
+        let appId = simengine.train_manager.register_station("Apple".to_string(), 0, 0);
+        let banId = simengine.train_manager.register_station("Banana".to_string(), 50, 0);
+        let cocId =  simengine.train_manager.register_station("Coconut".to_string(), 150, 0);
+        let draId = simengine.train_manager.register_station("Dragonfruit".to_string(), 150, 100);
+
+        let choId = simengine.train_manager.register_station("Chocolate".to_string(), 100, 100);
+        let milId = simengine.train_manager.register_station("Milk".to_string(), 200, 200);
+
+        let fruityId = simengine.train_manager.register_linetable("FRUITY".to_string(), 10);
+        let sweetyId = simengine.train_manager.register_linetable("SWEETY".to_string(), 3);
+        
+        // add station timetables
+        simengine.train_manager.add_station_toline(fruityId, StationTimeTable::new(appId, 5, Some(50), Some(banId)));
+        simengine.train_manager.add_station_toline(fruityId, StationTimeTable::new(banId, 6, Some(100), Some(cocId)));
+        simengine.train_manager.add_station_toline(fruityId, StationTimeTable::new(cocId, 7, Some(100), Some(draId)));
+        simengine.train_manager.add_station_toline(fruityId, StationTimeTable::new(draId, 8, None, None));
+        
+        simengine.train_manager.add_station_toline(sweetyId, StationTimeTable::new(choId, 10, Some(70), Some(cocId)));
+        simengine.train_manager.add_station_toline(sweetyId, StationTimeTable::new(cocId, 10, Some(70), Some(milId)));
+        simengine.train_manager.add_station_toline(sweetyId, StationTimeTable::new(milId, 10, None, None));
+
+        // expected results:
+        // train 0: (s0, Arr, 10), (s0, Dep, 15), (s1, Arr, 20), (s1, Dep, 26), (s2, Arr, 36), (s2, Dep, 43), (s3, Arr, 53), (s3, Dep, 61)
+        // train 1: (s0, Arr, 35), (s0, Dep, 40), (s1, Arr, 45), (s1, Dep, 51), (s2, Arr, 61), (s2, Dep, 68), (s3, Arr, 78), (s3, Dep, 86)
+        // train 2: (s4, Arr, 20), (s4, Dep, 30), (s2, Arr, 53), (s2, Dep, 63), (s5, Arr, 86), (s5, Dep, 96)
+        // train 3: (s4, Arr, 80), (s4, Dep, 90), (s2, Arr, 113), (s2, Dep, 123), (s5, Arr, 146), (s5, Dep, 156)
+
+        // new train will start from Station Apple on Time 0 and Time 75;
+        let new_times_fruity = vec![10, 35];
+        let new_times_sweety = vec![20, 80];
+
+        for time in new_times_fruity {
+            simengine.train_manager.add_new_traintime_toline(fruityId, time);
+            let ntid = simengine.train_manager.register_train(fruityId, appId, time);
+            simengine.spawn_train(time, fruityId, appId, ntid);
+        }
+
+        for time in new_times_sweety {
+            simengine.train_manager.add_new_traintime_toline(sweetyId, time);
+            let ntid = simengine.train_manager.register_train(sweetyId, choId, time);
+            simengine.spawn_train(time, sweetyId, choId, ntid);
+        }
+        
+        let mut clock = 0;
+        loop {
+            if clock >= 200 {
+                break;
+            }
+            simengine.do_step(clock);
+            clock += 1;
+        }
+    }
 }
